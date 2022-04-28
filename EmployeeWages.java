bfe7a1bb5562ae112ea083bf31b429c405f8db51
swapnilwaghmare24 <iamswapnilengg@gmail.com>
2022-04-28 23:35:01 +0530

    Daily wages calculated

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -2,16 +2,19 @@ package Com.Bridgelabz.EmpWages;
 
 public class EmployeeWages {
 
+
         private static final int IS_PRESENT = 1;
         private static final int IS_ABSENT = 0;
+        private static final int WAGE_PER_HOUR=20;
+        private static final int DAY_HOUR=8;
+
         public static void main(String[] args) {
-
-
-
+            int dailyWage=0;
             int empStatus = (int) (Math.random() * 10) % 2;
             switch (empStatus) {
                 case IS_PRESENT:
                     System.out.println(empStatus + " Employee is present");
+                    dailyWage = WAGE_PER_HOUR*DAY_HOUR;
                     break;
                 case IS_ABSENT:
                     System.out.println(empStatus + " Employee is absent");
@@ -19,5 +22,6 @@ public class EmployeeWages {
                 default:
                     System.out.println("invalid input");
                 }
+                System.out.println("Per day wages = "+dailyWage);
             }
 }
